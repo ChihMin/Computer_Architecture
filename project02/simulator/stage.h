64feee89717d8ddcab1ce5e76f5bf362b827283b
ChihMin <s102062111@m102.nthu.edu.tw>
2015-05-01 12:59:45 +0800

    add stage template

--- project02/simulator/stage.h
+++ project02/simulator/stage.h
@@ -1,29 +1,40 @@
-#include "simulator.h"
+#ifndef STAGE_H
+#define STAGE_H
+
+#include "interpreter.h"
 
 class Entry{
 
 public :
 	
-	Entry(Instruction _ins, u32 _fake_ins, bool _errorp[]);
+	Entry(const Instruction &_ins, u32 _fake_ins, bool _error[]);
 	Instruction get_ins();
 	u32 get_fake_ins();
 	bool get_error(int i);
+	int *get_reg();
+	void set_reg(const int reg[]);
+	void set_instruciton(const Instruction &ins);
 
 private :
 	
 	Instruction ins;
 	u32 fake_ins;
+	int reg[40];
 	bool error[5];
-}
+};
 
 class Stage{
 
 public :
 	
-	Stage();
+	Stage(const Instruction &NOP);
+	void push_ins(const Instruction &ins);
+	void insert_nop();
+	void flush_replace(const Instruction &ins);
 
 private :
 	
 	Entry stage[6];
-
 };
+
+#endif
